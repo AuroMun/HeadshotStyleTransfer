60b80d1e3265b434963359a6b5043a3380818b8c
erilyth <vishal.batchu@students.iiit.ac.in>
2016-11-04 00:00:40 +0530

    Add preliminary results and reorganize code Convert to CIE space before transformations and then back to RGB

--- Code/computegain.m
+++ Code/computegain.m
@@ -1,7 +1,8 @@
 function [gain] = computegain(energy1, energy2, levels)
     gauss_cur = 1;
     for k = 1:levels-1
-        gain{k} = sqrt(energy2{k} ./ (energy1{k} + 0.0000001));
+        gain{k} = sqrt(energy2{k} ./ (energy1{k} + 0.00001));
+        % Clamp the gains
         gain{k} = imgaussfilt(max(min(gain{k}, 2.8), 0.9), 3 * gauss_cur);
         gauss_cur = gauss_cur * 2;
     end
--- Code/styletransfer.m
+++ Code/styletransfer.m
@@ -1,7 +1,7 @@
 levels = 6;
 
-img_orig = imread('face1.png');
-img_style = imread('face2.png');
+img_orig = imread('Inputs/face1.png');
+img_style = imread('Inputs/face2.png');
 img_orig = imresize(img_orig, [300 NaN]);
 img_style = imresize(img_style, [size(img_orig,1) size(img_orig,2)]);
 img_orig = double(img_orig) / 255.0;
@@ -9,6 +9,9 @@ img_style = double(img_style) / 255.0;
 
 figure, imshow(uint8(img_orig * 255.0));
 figure, imshow(uint8(img_style * 255.0));
+
+img_orig = rgb2lab(img_orig) / 100.0;
+img_style = rgb2lab(img_style) / 100.0;
 
 % When we perform warping however, we should calculate the energy initially
 % and then for whatever warping is done, perform the corresponding changes
@@ -26,5 +29,7 @@ gain = computegain(energy1, energy2, levels);
 
 final_pyramid = robusttransfer(pyramid1, pyramid2, gain, levels);
 final_img = reconstructimage(final_pyramid, levels);
+
+final_img = lab2rgb(final_img * 100.0);
 
 figure, imshow(uint8(final_img * 255.0));
