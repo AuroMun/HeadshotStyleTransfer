279521d8e50bc6e050f9843e90fe4467cae0a0f2
erilyth <vishal.batchu@students.iiit.ac.in>
2016-11-04 18:43:45 +0530

    Use fspecial filters to convolve Process LAB images properly ensuring there are no negative values

--- Code/createpyramid.m
+++ Code/createpyramid.m
@@ -1,13 +1,12 @@
 function [pyramid] = createpyramid(img, levels)
     img_old = img;
     cur_gauss = 2;
-    img_cur = imgaussfilt(img,cur_gauss);
+    img_cur = imfilter(img, fspecial('gaussian', cur_gauss, cur_gauss));
     for k = 1:levels-1
         pyramid{k} = img_old - img_cur;
-        %figure, imshow(pyramid{k});
         img_old = img_cur;
         cur_gauss = cur_gauss * 2;
-        img_cur = imgaussfilt(img_cur,cur_gauss);
+        img_cur = imfilter(img, fspecial('gaussian', cur_gauss, cur_gauss));
     end
     pyramid{levels} = img_cur;
 end
