e4b38373a8e7068d4eaafd207a5dd00f370c59e6
erilyth <vishal.batchu@students.iiit.ac.in>
2016-11-24 21:07:30 +0530

    Add forehead to facial landmarks Assumption is that forehead landmarks are symmetric to the chin landmarks

--- Code/Face_Morph/aasthana_cvpr2013_code_version_2.0/align.m
+++ Code/Face_Morph/aasthana_cvpr2013_code_version_2.0/align.m
@@ -1,9 +1,31 @@
-p2 = detect('face1.png', 0);
-p1 = detect('face2.png', 0);
+p1 = detect('face1.png', 0);
+p2 = detect('face2.png', 0);
 
-figure, imshow(imread('face1.png'));
-figure, imshow(imread('face2.png'));
+img1 = imresize(imread('face1.png'), [300 230]);
+img2 = imresize(imread('face2.png'), [300 230]);
 
+new_points1 = zeros(66+13, 2);
+new_points2 = zeros(66+13, 2);
+for i=1:66
+    new_points1(i,:) = p1.points(i,:);
+    new_points2(i,:) = p2.points(i,:);
+end
+for i=1:13
+    new_points1(66+i,1) = p1.points(i+2,1);
+    new_points1(66+i,2) = p1.points(i+2,2)*-1 + 300;
+    new_points1(66+i,2) = new_points1(66+i,2) / exp((200 - new_points1(66+i,2))/300);
+    new_points1(66+i,2) = new_points1(66+i,2) + 25;
+    new_points2(66+i,1) = p2.points(i+2,1);
+    new_points2(66+i,2) = p2.points(i+2,2)*-1 + 300;
+    new_points2(66+i,2) = new_points2(66+i,2) / exp((200 - new_points2(66+i,2))/300);
+    new_points2(66+i,2) = new_points2(66+i,2) + 25;
+end
+p1.points = new_points1;
+p2.points = new_points2;
+figure, imshow(img2);
+axis image;
+hold on;
+plot(new_points2(:,1),new_points2(:,2),'r.')
 %Delaunay points of mean face points
 dis = (p1.points+p2.points)/2;
 x = dis(:, 1); y = dis(:, 2);
@@ -13,7 +35,7 @@ tri = delaunay(x,y);
 morphed_im = morph(p1.img*255, p2.img*255, p1.points, p2.points, tri, 1,0 , 0);
 
 %Show image%
-imshowpair(p2.img, morphed_im, 'montage');
+%figure, imshowpair(p2.img, morphed_im, 'montage');
 morphed_im = im2double(morphed_im);
 orig = p2.img;
 
